a39935b84289dd9f12c7303020824ac98ad11721
rezaplus <rezahajziade22@gmail.com>
2024-08-18 10:12:35 +0300

    fix role middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -15,7 +16,7 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $role)
     {
         if (!Auth::check() || !Auth::user()->hasRole($role)) {
             return response()->json(['message' => 'Sorry, you are not authorized to access this resource'], 403);
